f06a39481739e2f40fb7d7c711be0078d978fc37
lac223 <lac223@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2010-12-15 20:47:28 +0000

    CIN
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/analysis-server/trunk@1248 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- R_source/heatmap.draw.R
+++ R_source/heatmap.draw.R
@@ -1,3 +1,6 @@
+## cin: regular CIN matrix, each row is a chromosome, each column is a sample(patient)
+## clinical.inf: n*2 matrix, the 1st column is 'sample name', the second is 'label'
+
 'heatmap.draw'<-function(cin, clinical.inf, heatmap.title='Chromosome instability index'){
 	require(gplots)
 	labels=matrix(clinical.inf[,2])
@@ -6,6 +9,11 @@
 	idx.sort=order(labels)
 	samplenames=samplenames[idx.sort]
 	cin=cin[,samplenames]
+	
+	for(i in 1:nrow(cin)) {
+		if(sum(cin[i,])==0)
+			cin[i,]=cin[i,]+rnorm(ncol(cin))/10
+	}
 	
 	u.labels=unique(labels.sort)
 	n.labels=length(u.labels)
@@ -16,11 +24,12 @@
 		col.cin=c(col.cin, rep(color.pool[i], n.curcol))
 	}
 	dev=png(file=paste(heatmap.title, '.png',sep=''))
-	main.title=paste(heatmap.title, ' heatmap',sep='')
+	#main.title=paste(heatmap.title, ' heatmap',sep='')
+	main.title='CIN heatmap'
 	for (i in 1:n.labels) {
 		main.title=paste(main.title,'\n', color.pool[i], ' : ', u.labels[i])
 	}
-	heatmap.2(cin,ColSideColors=col.cin,col='greenred',dendrogram='none',Colv=FALSE,Rowv=FALSE, scale='row', trace='none', main=main.title)
+	heatmap.3(t(cin),RowSideColors=col.cin,col='greenred',dendrogram='none',Colv=FALSE,Rowv=FALSE, scale='column', trace='none', main=main.title)
 #	browser()
 	dev.off()
 }
