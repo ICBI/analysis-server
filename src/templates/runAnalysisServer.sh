9601a0e4b8e75700bfd6234d9c23e51ea7e53d1b
acs224 <acs224@7e94895d-eddb-4489-9ea7-1067dfd99f16>
2011-04-05 19:12:23 +0000

    fixing script
    git-svn-id: https://svn-bccfr.uis.georgetown.edu/svn/gdoc/analysis-server/trunk@1629 7e94895d-eddb-4489-9ea7-1067dfd99f16

--- src/templates/runAnalysisServer.sh
+++ src/templates/runAnalysisServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup $JAVA_HOME/bin/java -Xmx512m -Xms128m -classpath ./:../lib/concurrent.jar:../lib/jbossall-client.jar:../lib/log4j.jar:../lib/Rserve.jar:../lib/REngine.jar:../lib/caintegrator-analysis-commons.jar:../lib/caintegrator-analysis-server.jar gov.nih.nci.caintegrator.analysis.server.AnalysisServer  analysisServer.properties &
+nohup java -Xmx512m -Xms128m -classpath ./:../lib/concurrent.jar:../lib/jbossall-client.jar:../lib/log4j.jar:../lib/Rserve.jar:../lib/REngine.jar:../lib/caintegrator-analysis-commons.jar:../lib/caintegrator-analysis-server.jar gov.nih.nci.caintegrator.analysis.server.AnalysisServer  analysisServer.properties &
